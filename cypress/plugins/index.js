2f0660a11b33e8de07abfd0a251f400e4f086975
“SalvaCastany” <salvadorcastany@gmail.com>
2023-02-03 07:38:50 +0100

    Adapt to TS

---
+++ cypress/plugins/index.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+const wp = require('@cypress/webpack-preprocessor')
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  const options = {
+    webpackOptions: require('../../webpack.config'),
+  }
+
+  on('file:preprocessor', wp(options))
+}
